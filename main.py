2a9a52887122906ce27926070486f991ffcca2bd
WilliamFerreira <williamroberto.crow@gmail.com>
2023-01-22 20:03:39 -0300

    refactor: code generalization
    made a for in the first three gpu in 1660 block,

--- main.py
+++ main.py
@@ -45,17 +45,17 @@ firstinteraction = [ordenar, precocrescente, cookieaccept]
 functionrepeat(site, firstinteraction)
 
 # GPUVRAM ID's
-tengb = '//*[@id="asideFilters"]/div[2]/details[7]/div/label[2]'
-twelvegb = '//*[@id="asideFilters"]/div[2]/details[7]/div/label[3]'
-sixteengb = '//*[@id="asideFilters"]/div[2]/details[7]/div/label[4]'
-twentygb = '//*[@id="asideFilters"]/div[2]/details[7]/div/label[6]'
-twentyfourgb = '//*[@id="asideFilters"]/div[2]/details[7]/div/label[7]'
-sixgb = '//*[@id="asideFilters"]/div[2]/details[7]/div/label[9]'
-eightgb = '//*[@id="asideFilters"]/div[2]/details[7]/div/label[10]'
-seemoregpuvram = '//*[@id="asideFilters"]/div[2]/details[7]/div/span'
+ten_gb = '//*[@id="asideFilters"]/div[2]/details[7]/div/label[2]'
+twelve_gb = '//*[@id="asideFilters"]/div[2]/details[7]/div/label[3]'
+sixteen_gb = '//*[@id="asideFilters"]/div[2]/details[7]/div/label[4]'
+twenty_gb = '//*[@id="asideFilters"]/div[2]/details[7]/div/label[6]'
+twenty_four_gb = '//*[@id="asideFilters"]/div[2]/details[7]/div/label[7]'
+six_gb = '//*[@id="asideFilters"]/div[2]/details[7]/div/label[9]'
+eight_gb = '//*[@id="asideFilters"]/div[2]/details[7]/div/label[10]'
+see_more_gpu_video_ram = '//*[@id="asideFilters"]/div[2]/details[7]/div/span'
 
-# list with all the vpuvram ids
-# gpuvramlist = [tengb,twelvegb,sixteengb,seemoregpuvram,twentygb,twentyfourgb,sixgb,eightgb]
+# list with all the GPU video ram
+# gpu_video_ram_list = [ten_gb,twelve_gb,sixteen_gb,see_more_gpu_video_ram,twenty_gb,twenty_four_gb,six_gb,eight_gb]
 
 # #calling functionrepeat
 # functionrepeat(site, gpuvramlist)
@@ -71,44 +71,22 @@ seemoregpuvram = '//*[@id="asideFilters"]/div[2]/details[7]/div/span'
 # separated the first gpu by blocks cause the site could be broke if you use all the checkbox we need
 # the first block of gpu info
 all_gpu_list_1660 = []
+for i in range(1,4):
+    gpu_name_xpath = f'//*[@id="listing"]/div[3]/div/div[2]/div[1]/main/div[{i}]/a/div/button/div/h2/span'
+    gpu_price_xpath = f'//*[@id="listing"]/div[3]/div/div[2]/div[1]/main/div[{i}]/a/div/div/span[2]'
+    gpu_name = site.find_element(By.XPATH, gpu_name_xpath).text
+    gpu_price = site.find_element(By.XPATH, gpu_price_xpath).text
+    site.find_element(By.XPATH, gpu_name_xpath).click()
+    current_url = site.current_url
+    gpu_link = current_url
+    all_gpu_list_1660.append([gpu_name, gpu_price, gpu_link])
+    site.back()
 
-first_gpu_name_xpath = '//*[@id="listing"]/div[3]/div/div[2]/div[1]/main/div[1]/a/div/button/div/h2/span'
-first_gpu_price_xpath = '//*[@id="listing"]/div[3]/div/div[2]/div[1]/main/div[1]/a/div/div/span[2]'
-first_gpu_name = site.find_element(By.XPATH, first_gpu_name_xpath).text
-first_gpu_price = site.find_element(By.XPATH, first_gpu_price_xpath).text
-site.find_element(By.XPATH, first_gpu_name_xpath).click()
-current_url = site.current_url
-gpu_link = current_url
-site.back()
-
-all_gpu_list_1660.append([first_gpu_name, first_gpu_price, gpu_link])
-
-
-
-# second block of gpu info
-second_gpu_name_xpath = '//*[@id="listing"]/div[3]/div/div[2]/div[1]/main/div[2]/a/div/button/div/h2/span'
-second_gpu_price_xpath = '//*[@id="listing"]/div[3]/div/div[2]/div[1]/main/div[2]/a/div/div/span[2]'
-second_gpu_name = site.find_element(By.XPATH, second_gpu_name_xpath).text
-second_gpu_price = site.find_element(By.XPATH, second_gpu_price_xpath).text
-site.find_element(By.XPATH, second_gpu_name_xpath).click()
-current_url = site.current_url
-gpu_link = current_url
-site.back()
-all_gpu_list_1660.append([second_gpu_name, second_gpu_price, gpu_link])
-
-
-#third block of gpu info
-third_gpu_name_xpath = '//*[@id="listing"]/div[3]/div/div[2]/div[1]/main/div[3]/a/div/button/div/h2/span'
-third_gpu_price_xpath = '//*[@id="listing"]/div[3]/div/div[2]/div[1]/main/div[3]/a/div/div/span[2]'
-third_gpu_name = site.find_element(By.XPATH, third_gpu_name_xpath).text
-third_gpu_price = site.find_element(By.XPATH, third_gpu_price_xpath).text
-site.find_element(By.XPATH, third_gpu_name_xpath).click()
-current_url = site.current_url
-gpu_link = current_url
-site.back()
-all_gpu_list_1660.append([third_gpu_name, third_gpu_price, gpu_link])
-
-print(*all_gpu_list_1660,sep='\n')
+# separating all the three gpu and show in list
+for l in all_gpu_list_1660:
+    for i in l:
+        print(i)
+    print()
 
 
 
