373b77935c5b19ad58cc54fff7a2d3995956adf8
WilliamFerreira <williamroberto.crow@gmail.com>
2023-01-22 19:15:54 -0300

    feat: three new block of gpu info
    these three new blocks contains names, prices and url all in one list.

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from time import sleep
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.action_chains import ActionChains
 
-#that validation is used to check xpath everytime is called
+
+# that validation is used to check xpath everytime is called
 def validacaoxpath(site, xpath):
     try:
         site.find_element(By.XPATH, xpath)
@@ -16,7 +16,8 @@ def validacaoxpath(site, xpath):
         return False
     return True
 
-#for with an validation for every xpath, that's prevent when the page is not loaded and can't find the xpath div
+
+# for with an validation for every xpath, that's prevent when the page is not loaded and can't find the xpath div
 def functionrepeat(site, functionlist):
     for i in functionlist:
         while not validacaoxpath(site, i):
@@ -24,6 +25,7 @@ def functionrepeat(site, functionlist):
         funcao = site.find_element(By.XPATH, i)
         funcao.click()
         sleep(3)
+
 
 servico = Service(ChromeDriverManager().install())
 site = webdriver.Chrome(service=servico)
@@ -36,13 +38,13 @@ ordenar = '//*[@id="Filter"]/div[1]/select'
 precocrescente = '//*[@id="Filter"]/div[1]/select/option[2]'
 cookieaccept = '//*[@id="onetrust-accept-btn-handler"]'
 
-#first interaction list contains the first step when the site is loaded
-firstinteraction = [ordenar,precocrescente,cookieaccept]
+# first interaction list contains the first step when the site is loaded
+firstinteraction = [ordenar, precocrescente, cookieaccept]
 
-#calling function repeat
+# calling function repeat
 functionrepeat(site, firstinteraction)
 
-#GPUVRAM ID's
+# GPUVRAM ID's
 tengb = '//*[@id="asideFilters"]/div[2]/details[7]/div/label[2]'
 twelvegb = '//*[@id="asideFilters"]/div[2]/details[7]/div/label[3]'
 sixteengb = '//*[@id="asideFilters"]/div[2]/details[7]/div/label[4]'
@@ -52,7 +54,7 @@ sixgb = '//*[@id="asideFilters"]/div[2]/details[7]/div/label[9]'
 eightgb = '//*[@id="asideFilters"]/div[2]/details[7]/div/label[10]'
 seemoregpuvram = '//*[@id="asideFilters"]/div[2]/details[7]/div/span'
 
-# #list with all the vpuvram ids
+# list with all the vpuvram ids
 # gpuvramlist = [tengb,twelvegb,sixteengb,seemoregpuvram,twentygb,twentyfourgb,sixgb,eightgb]
 
 # #calling functionrepeat
@@ -65,19 +67,48 @@ seemoregpuvram = '//*[@id="asideFilters"]/div[2]/details[7]/div/span'
 # gpublocka = [seemoreegputypetensixty, sixteensixtyti, sixteensixtysuper]
 # functionrepeat(site, gpublocka)
 
-#separated the first gpu by blocks cause the site could be broke if you use all the checkbox we need
-gpuname= '//*[@id="listing"]/div[3]/div/div[2]/div[1]/main/div[1]/a/div/button/div/h2/span'
-gpuprice = '//*[@id="listing"]/div[3]/div/div[2]/div[1]/main/div[1]/a/div/div/span[2]'
-firstgpuname = site.find_element(By.XPATH, gpuname).text
-firstgpuprice = site.find_element(By.XPATH, gpuprice).text
-firstgpuclick = site.find_element(By.XPATH, gpuname)
-firstgpuclick.click()
-urlatual = site.current_url
-firstgpulink = urlatual
+
+# separated the first gpu by blocks cause the site could be broke if you use all the checkbox we need
+# the first block of gpu info
+all_gpu_list_1660 = []
+
+first_gpu_name_xpath = '//*[@id="listing"]/div[3]/div/div[2]/div[1]/main/div[1]/a/div/button/div/h2/span'
+first_gpu_price_xpath = '//*[@id="listing"]/div[3]/div/div[2]/div[1]/main/div[1]/a/div/div/span[2]'
+first_gpu_name = site.find_element(By.XPATH, first_gpu_name_xpath).text
+first_gpu_price = site.find_element(By.XPATH, first_gpu_price_xpath).text
+site.find_element(By.XPATH, first_gpu_name_xpath).click()
+current_url = site.current_url
+gpu_link = current_url
 site.back()
 
+all_gpu_list_1660.append([first_gpu_name, first_gpu_price, gpu_link])
 
 
+
+# second block of gpu info
+second_gpu_name_xpath = '//*[@id="listing"]/div[3]/div/div[2]/div[1]/main/div[2]/a/div/button/div/h2/span'
+second_gpu_price_xpath = '//*[@id="listing"]/div[3]/div/div[2]/div[1]/main/div[2]/a/div/div/span[2]'
+second_gpu_name = site.find_element(By.XPATH, second_gpu_name_xpath).text
+second_gpu_price = site.find_element(By.XPATH, second_gpu_price_xpath).text
+site.find_element(By.XPATH, second_gpu_name_xpath).click()
+current_url = site.current_url
+gpu_link = current_url
+site.back()
+all_gpu_list_1660.append([second_gpu_name, second_gpu_price, gpu_link])
+
+
+#third block of gpu info
+third_gpu_name_xpath = '//*[@id="listing"]/div[3]/div/div[2]/div[1]/main/div[3]/a/div/button/div/h2/span'
+third_gpu_price_xpath = '//*[@id="listing"]/div[3]/div/div[2]/div[1]/main/div[3]/a/div/div/span[2]'
+third_gpu_name = site.find_element(By.XPATH, third_gpu_name_xpath).text
+third_gpu_price = site.find_element(By.XPATH, third_gpu_price_xpath).text
+site.find_element(By.XPATH, third_gpu_name_xpath).click()
+current_url = site.current_url
+gpu_link = current_url
+site.back()
+all_gpu_list_1660.append([third_gpu_name, third_gpu_price, gpu_link])
+
+print(*all_gpu_list_1660,sep='\n')
 
 
 
@@ -95,34 +126,12 @@ site.back()
 # seemoregputypethirty = '//*[@id="asideFilters"]/div[2]/details[9]/div/span'
 #
 #
-# #list with all gputype
+# list with all gputype
 # gputypelist =[rtxtwentysixty,rtxtwentysixtysuper,rtxtwentyeighty,
 #               rtxthirtysixty,rtxthirtysixtyti,rtxthirtyseventy,rtxthirtyseventyti,seemoregputypethirty,rtxthirtyeighty,
 #               rtxthirtyeightyti,rtxthirtyninety,rtxthirtyninetyti]
 #
-# #calling functionrepeat
+# calling functionrepeat
 # functionrepeat(site, gputypelist)
 
 sleep(500)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
