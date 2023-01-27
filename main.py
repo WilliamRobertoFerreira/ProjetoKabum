166c0584fa84eb04ed01c886f42974d031a759ee
WilliamFerreira <williamroberto.crow@gmail.com>
2023-01-27 18:00:40 -0300

    feat: new block for gpu 2000 search
    made a for in the first three gpu in 2000 block,

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ def validacaoxpath(site, xpath):
 
 
 # for with an validation for every xpath, that's prevent when the page is not loaded and can't find the xpath div
-def functionrepeat(site, functionlist):
-    for i in functionlist:
+def function_repeat(site, function_list):
+    for i in function_list:
         while not validacaoxpath(site, i):
             sleep(1)
         funcao = site.find_element(By.XPATH, i)
@@ -31,18 +31,20 @@ servico = Service(ChromeDriverManager().install())
 site = webdriver.Chrome(service=servico)
 
 # site.get('https://www.kabum.com.br/hardware/placa-de-video-vga')
-site.get('https://www.kabum.com.br/hardware/placa-de-video-vga?page_number=1&page_size=20&facet_filters=eyJNZW3Ds3JpYSI6WyIxMCBHQiIsIjEyIEdCIiwiMTYgR0IiLCIyMCBHQiIsIjI0IEdCIiwiNiBHQiIsIjggR0IiXSwiR2VGb3JjZSBHVFggU8OpcmllIDE2IjpbIkdUWCAxNjYwIFRpIiwiR1RYIDE2NjAgU3VwZXIiXX0=&sort=price')
-acoes = ActionChains(site)
+site.get('https://www.kabum.com.br/hardware/placa-de-video-vga?page_number=1&page_size=20&facet_filters'
+         '=eyJNZW3Ds3JpYSI6WyIxMCBHQiIsIjEyIEdCIiwiMTYgR0IiLCIyMCBHQiIsIjI0IEdCIiwiNiBHQiIsIjggR0IiXSwiR2VGb3JjZSBSVFgg'
+         'U8OpcmllIDIwIjpbIlJUWCAyMDgwIFN1cGVyIiwiUlRYIDIwNjAgU3VwZXIiLCJSVFggMjA2MCJdfQ==&sort=price')
+site_actions = ActionChains(site)
 
-ordenar = '//*[@id="Filter"]/div[1]/select'
-precocrescente = '//*[@id="Filter"]/div[1]/select/option[2]'
-cookieaccept = '//*[@id="onetrust-accept-btn-handler"]'
+order_by = '//*[@id="Filter"]/div[1]/select'
+preco_crescente = '//*[@id="Filter"]/div[1]/select/option[2]'
+cookie_accept = '//*[@id="onetrust-accept-btn-handler"]'
 
 # first interaction list contains the first step when the site is loaded
-firstinteraction = [ordenar, precocrescente, cookieaccept]
+first_interaction = [order_by, preco_crescente, cookie_accept]
 
 # calling function repeat
-functionrepeat(site, firstinteraction)
+function_repeat(site, first_interaction)
 
 # GPUVRAM ID's
 ten_gb = '//*[@id="asideFilters"]/div[2]/details[7]/div/label[2]'
@@ -57,21 +59,44 @@ see_more_gpu_video_ram = '//*[@id="asideFilters"]/div[2]/details[7]/div/span'
 # list with all the GPU video ram
 # gpu_video_ram_list = [ten_gb,twelve_gb,sixteen_gb,see_more_gpu_video_ram,twenty_gb,twenty_four_gb,six_gb,eight_gb]
 
-# #calling functionrepeat
-# functionrepeat(site, gpuvramlist)
+# #calling function_repeat
+# function_repeat(site, gpuvramlist)
 
 # #GPUTYPE
-# sixteensixtyti = '//*[@id="asideFilters"]/div[2]/details[11]/div/label[6]'
-# sixteensixtysuper = '//*[@id="asideFilters"]/div[2]/details[11]/div/label[5]'
-# seemoreegputypetensixty = '//*[@id="asideFilters"]/div[2]/details[11]/div/span'
-# gpublocka = [seemoreegputypetensixty, sixteensixtyti, sixteensixtysuper]
-# functionrepeat(site, gpublocka)
+# sixteen_sixty_ti = '//*[@id="asideFilters"]/div[2]/details[11]/div/label[6]'
+# sixteen_sixty_super = '//*[@id="asideFilters"]/div[2]/details[11]/div/label[5]'
+# see_more_gpu_type_ten_sixty = '//*[@id="asideFilters"]/div[2]/details[11]/div/span'
+# gpu_block_a = [see_more_gpu_type_ten_sixty, sixteen_sixty_ti, sixteen_sixty_super]
+# function_repeat(site, gpu_block_a)
 
 
 # separated the first gpu by blocks cause the site could be broke if you use all the checkbox we need
 # the first block of gpu info
-all_gpu_list_1660 = []
-for i in range(1,4):
+# all_gpu_list_1660 = []
+# for i in range(1, 4):
+#     gpu_name_xpath = f'//*[@id="listing"]/div[3]/div/div[2]/div[1]/main/div[{i}]/a/div/button/div/h2/span'
+#     gpu_price_xpath = f'//*[@id="listing"]/div[3]/div/div[2]/div[1]/main/div[{i}]/a/div/div/span[2]'
+#     gpu_name = site.find_element(By.XPATH, gpu_name_xpath).text
+#     gpu_price = site.find_element(By.XPATH, gpu_price_xpath).text
+#     site.find_element(By.XPATH, gpu_name_xpath).click()
+#     current_url = site.current_url
+#     gpu_link = current_url
+#     all_gpu_list_1660.append([gpu_name, gpu_price, gpu_link])
+#     site.back()
+#
+# # separating all the three gpu and show in list
+# for l in all_gpu_list_1660:
+#     for i in l:
+#         print(i)
+#     print()
+
+
+rtx_2060 = '//*[@id="asideFilters"]/div[2]/details[15]/div/label[1]'
+rtx_2060_super = '//*[@id="asideFilters"]/div[2]/details[15]/div/label[2]'
+rtx_2080 = '//*[@id="asideFilters"]/div[2]/details[15]/div/label[3]'
+
+all_gpu_list_2000 = []
+for i in range(1, 4):
     gpu_name_xpath = f'//*[@id="listing"]/div[3]/div/div[2]/div[1]/main/div[{i}]/a/div/button/div/h2/span'
     gpu_price_xpath = f'//*[@id="listing"]/div[3]/div/div[2]/div[1]/main/div[{i}]/a/div/div/span[2]'
     gpu_name = site.find_element(By.XPATH, gpu_name_xpath).text
@@ -79,22 +104,16 @@ for i in range(1,4):
     site.find_element(By.XPATH, gpu_name_xpath).click()
     current_url = site.current_url
     gpu_link = current_url
-    all_gpu_list_1660.append([gpu_name, gpu_price, gpu_link])
+    all_gpu_list_2000.append([gpu_name, gpu_price, gpu_link])
     site.back()
 
-# separating all the three gpu and show in list
-for l in all_gpu_list_1660:
+for l in all_gpu_list_2000:
     for i in l:
         print(i)
     print()
 
-
-
-# rtxtwentysixty = '//*[@id="asideFilters"]/div[2]/details[15]/div/label[1]'
-# rtxtwentysixtysuper = '//*[@id="asideFilters"]/div[2]/details[15]/div/label[2]'
-# rtxtwentyeighty = '//*[@id="asideFilters"]/div[2]/details[15]/div/label[3]'
-# rtxthirtysixty = '//*[@id="asideFilters"]/div[2]/details[9]/div/label[2]'
-# rtxthirtysixtyti = '//*[@id="asideFilters"]/div[2]/details[9]/div/label[3]'
+# rtx_thirty_sixty = '//*[@id="asideFilters"]/div[2]/details[9]/div/label[2]'
+# rtx_thirty_sixty_ti = '//*[@id="asideFilters"]/div[2]/details[9]/div/label[3]'
 # rtxthirtyseventy = '//*[@id="asideFilters"]/div[2]/details[9]/div/label[4]'
 # rtxthirtyseventyti = '//*[@id="asideFilters"]/div[2]/details[9]/div/label[5]'
 # rtxthirtyeighty = '//*[@id="asideFilters"]/div[2]/details[9]/div/label[6]'
@@ -110,6 +129,6 @@ for l in all_gpu_list_1660:
 #               rtxthirtyeightyti,rtxthirtyninety,rtxthirtyninetyti]
 #
 # calling functionrepeat
-# functionrepeat(site, gputypelist)
+# function_repeat(site, gputypelist)
 
 sleep(500)
