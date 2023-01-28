8bef42df96b41aa4f36f8b878af554f420b397ac
WilliamFerreira <williamroberto.crow@gmail.com>
2023-01-27 21:06:52 -0300

    feat: new block of mining and code generalization
    code generalization in all variables
    created new def to show list information
    created unselected method to clean the page
    mining data at rtx series 30
    finishing the code

--- main.py
+++ main.py
@@ -26,40 +26,44 @@ def function_repeat(site, function_list):
         funcao.click()
         sleep(3)
 
+
 # in this function we click on the first 3 gpus separated by type and save all the information we need in one list
 def data_crawler(site, data_list):
     for i in range(1, 4):
+        sleep(2)
         gpu_name_xpath = f'//*[@id="listing"]/div[3]/div/div[2]/div[1]/main/div[{i}]/a/div/button/div/h2/span'
         gpu_price_xpath = f'//*[@id="listing"]/div[3]/div/div[2]/div[1]/main/div[{i}]/a/div/div/span[2]'
         gpu_name = site.find_element(By.XPATH, gpu_name_xpath).text
         gpu_price = site.find_element(By.XPATH, gpu_price_xpath).text
+        sleep(2)
         site.find_element(By.XPATH, gpu_name_xpath).click()
+        sleep(2)
         current_url = site.current_url
         gpu_link = current_url
         data_list.append([gpu_name, gpu_price, gpu_link])
         site.back()
+        sleep(1)
 
+
+def show_lists(data_list):
     for l in data_list:
         for i in l:
             print(i)
         print()
 
 
-servico = Service(ChromeDriverManager().install())
-site = webdriver.Chrome(service=servico)
+driver_service = Service(ChromeDriverManager().install())
+site = webdriver.Chrome(service=driver_service)
 
 site.get('https://www.kabum.com.br/hardware/placa-de-video-vga')
-# site.get('https://www.kabum.com.br/hardware/placa-de-video-vga?page_number=1&page_size=20&facet_filters'
-#          '=eyJNZW3Ds3JpYSI6WyIxMCBHQiIsIjEyIEdCIiwiMTYgR0IiLCIyMCBHQiIsIjI0IEdCIiwiNiBHQiIsIjggR0IiXSwiR2VGb3JjZSBSVFgg'
-#          'U8OpcmllIDIwIjpbIlJUWCAyMDgwIFN1cGVyIiwiUlRYIDIwNjAgU3VwZXIiLCJSVFggMjA2MCJdfQ==&sort=price')
 site_actions = ActionChains(site)
 
 order_by = '//*[@id="Filter"]/div[1]/select'
-preco_crescente = '//*[@id="Filter"]/div[1]/select/option[2]'
+price_asc = '//*[@id="Filter"]/div[1]/select/option[2]'
 cookie_accept = '//*[@id="onetrust-accept-btn-handler"]'
 
 # first interaction list contains the first step when the site is loaded
-first_interaction = [order_by, preco_crescente, cookie_accept]
+first_interaction = [order_by, price_asc, cookie_accept]
 
 # calling function repeat
 function_repeat(site, first_interaction)
@@ -81,24 +85,25 @@ gpu_video_ram_list = [ten_gb, twelve_gb, sixteen_gb, see_more_gpu_video_ram, twe
 # calling function_repeat
 function_repeat(site, gpu_video_ram_list)
 
-## all series 1660 we need to mine info
+# all series 1660 we need to mine info
 gpu_1660 = '//*[@id="asideFilters"]/div[2]/details[12]/div/label[4]'
 gpu_1660_super = '//*[@id="asideFilters"]/div[2]/details[12]/div/label[5]'
 gpu_1660_ti = '//*[@id="asideFilters"]/div[2]/details[12]/div/label[6]'
 see_more_gpu_type_1660 = '//*[@id="asideFilters"]/div[2]/details[12]/div/span'
 
 # rtx_1660 series we need for function repeat and click
-gpu_series_1660 = [gpu_1660, gpu_1660_super, see_more_gpu_type_1660, gpu_1660_ti]
-function_repeat(site, gpu_series_1660)
+gtx_series_1660 = [gpu_1660, gpu_1660_super, see_more_gpu_type_1660, gpu_1660_ti]
+function_repeat(site, gtx_series_1660)
 
 # list for data crawler function call
 all_gpu_list_1660 = []
 data_crawler(site, all_gpu_list_1660)
 
-#removing 1660 series to call another one
-function_repeat(site, gpu_series_1660)
+# removing 1660 series to call another one
+gtx_series_1660_unselect = [gpu_1660, gpu_1660_super, gpu_1660_ti]
+function_repeat(site, gtx_series_1660_unselect)
 
-# all series 2000 we need to mine inf
+# all series 2000 we need to mine info
 rtx_2060 = '//*[@id="asideFilters"]/div[2]/details[17]/div/label[1]'
 rtx_2060_super = '//*[@id="asideFilters"]/div[2]/details[17]/div/label[2]'
 rtx_2080 = '//*[@id="asideFilters"]/div[2]/details[17]/div/label[3]'
@@ -111,26 +116,41 @@ function_repeat(site, rtx_series_2000)
 all_gpu_list_2000 = []
 data_crawler(site, all_gpu_list_2000)
 
-#removing 2000 series to call another one
+# removing series 20 to call another one
 function_repeat(site, rtx_series_2000)
 
-# rtx_thirty_sixty = '//*[@id="asideFilters"]/div[2]/details[9]/div/label[2]'
-# rtx_thirty_sixty_ti = '//*[@id="asideFilters"]/div[2]/details[9]/div/label[3]'
-# rtxthirtyseventy = '//*[@id="asideFilters"]/div[2]/details[9]/div/label[4]'
-# rtxthirtyseventyti = '//*[@id="asideFilters"]/div[2]/details[9]/div/label[5]'
-# rtxthirtyeighty = '//*[@id="asideFilters"]/div[2]/details[9]/div/label[6]'
-# rtxthirtyeightyti = '//*[@id="asideFilters"]/div[2]/details[9]/div/label[7]'
-# rtxthirtyninety = '//*[@id="asideFilters"]/div[2]/details[9]/div/label[8]'
-# rtxthirtyninetyti = '//*[@id="asideFilters"]/div[2]/details[9]/div/label[9]'
-# seemoregputypethirty = '//*[@id="asideFilters"]/div[2]/details[9]/div/span'
-#
-#
-# list with all gputype
-# gputypelist =[rtxtwentysixty,rtxtwentysixtysuper,rtxtwentyeighty,
-#               rtxthirtysixty,rtxthirtysixtyti,rtxthirtyseventy,rtxthirtyseventyti,seemoregputypethirty,rtxthirtyeighty,
-#               rtxthirtyeightyti,rtxthirtyninety,rtxthirtyninetyti]
-#
-# calling functionrepeat
-# function_repeat(site, gputypelist)
+# all series 30 we need to mine info
+rtx_3060 = '//*[@id="asideFilters"]/div[2]/details[9]/div/label[2]'
+rtx_3060_ti = '//*[@id="asideFilters"]/div[2]/details[9]/div/label[3]'
+rtx_3070 = '//*[@id="asideFilters"]/div[2]/details[9]/div/label[4]'
+rtx_3070_ti = '//*[@id="asideFilters"]/div[2]/details[9]/div/label[5]'
+see_more_gpu_type_3000 = '//*[@id="asideFilters"]/div[2]/details[9]/div/span'
+rtx_3080 = '//*[@id="asideFilters"]/div[2]/details[9]/div/label[6]'
+rtx_3080_ti = '//*[@id="asideFilters"]/div[2]/details[9]/div/label[7]'
+rtx_3090 = '//*[@id="asideFilters"]/div[2]/details[9]/div/label[8]'
+rtx_3090_ti = '//*[@id="asideFilters"]/div[2]/details[9]/div/label[9]'
 
-sleep(500)
+# list with all rtx series 30
+rtx_series_3000 = [rtx_3060, rtx_3060_ti, rtx_3070, rtx_3070_ti, see_more_gpu_type_3000,
+                   rtx_3080, rtx_3080_ti, rtx_3090, rtx_3090_ti]
+
+# calling function_repeat
+function_repeat(site, rtx_series_3000)
+
+# calling function data crawler to mine all the information and save in all_gpu_list_3000
+all_gpu_list_3000 = []
+data_crawler(site, all_gpu_list_3000)
+
+# calling function_repeat do unselect all the info we don't need anymore
+rtx_series_3000_unselect = [rtx_3060, rtx_3060_ti, rtx_3070, rtx_3070_ti,
+                            rtx_3080, rtx_3080_ti, rtx_3090, rtx_3090_ti]
+function_repeat(site, rtx_series_3000_unselect)
+
+print('All series 16 with values and links: ')
+show_lists(all_gpu_list_1660)
+
+print('All Series 20 with values and links: ')
+show_lists(all_gpu_list_2000)
+
+print('All series 30 with values and links: ')
+show_lists(all_gpu_list_3000)
